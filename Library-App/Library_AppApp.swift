29afcb12f98e47e6cb73ff48477e4d8c7fba910d
callum day ham <caldayham@gmail.com>
2021-05-28 11:07:40 -0700

    Initial Commit

---
+++ Library-App/Library_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Library_AppApp.swift
+//  Library-App
+//
+//  Created by Callum Day Ham on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Library_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
